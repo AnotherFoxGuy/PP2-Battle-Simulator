565ef20526bca0cd1c5e412dcd0d388585e512cf
Edgar <Edgar@AnotherFoxGuy.com>
2019-12-17 13:08:36 +0100

    SPeed

--- source/game.cpp
+++ source/game.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #include <iostream>
 #include "surface.h"
 #include "template.h"
+#include "tbb/parallel_sort.h"
 
 using namespace PP2;
 
@@ -41,7 +42,8 @@ using namespace PP2;
 // Roland:  69931
 // Sietze:  65732
 // Gert:    73804
-#define REF_PERFORMANCE 65732 //UPDATE THIS WITH YOUR REFERENCE PERFORMANCE (see console after 2k frames)
+#define REF_PERFORMANCE 69338 //UPDATE THIS WITH YOUR REFERENCE PERFORMANCE (see console after 2k frames)
+
 static timer perf_timer;
 static float duration;
 
@@ -270,8 +272,18 @@ void Game::Draw() {
         const UINT16 NUM_TANKS = ((t < 1) ? NUM_TANKS_BLUE : NUM_TANKS_RED);
 
         const UINT16 begin = ((t < 1) ? 0 : NUM_TANKS_BLUE);
-        std::vector<const Tank *> sorted_tanks;
-        insertion_sort_tanks_health(tanks, sorted_tanks, begin, begin + NUM_TANKS);
+
+        //insertion_sort_tanks_health(tanks, sorted_tanks, begin, begin + NUM_TANKS);
+
+        auto ibegin = tanks.begin() + begin;
+        auto ilast = tanks.begin() + (begin + NUM_TANKS);
+        std::vector<Tank> sorted_tanks(ibegin, ilast);
+
+        //std::vector<const Tank *> sorted_tanks(tanks(begin), begin + );
+
+        tbb::parallel_sort(sorted_tanks, [](const Tank &a, const Tank &b) -> bool {
+            return a.health < b.health;
+        });
 
         for (int i = 0; i < NUM_TANKS; i++) {
             int health_bar_start_x = i * (HEALTH_BAR_WIDTH + HEALTH_BAR_SPACING) + HEALTH_BARS_OFFSET_X;
@@ -282,7 +294,7 @@ void Game::Draw() {
             screen->Bar(health_bar_start_x, health_bar_start_y, health_bar_end_x, health_bar_end_y, REDMASK);
             screen->Bar(health_bar_start_x, health_bar_start_y + (int) ((double) HEALTH_BAR_HEIGHT * (1 -
                                                                                                       ((double) sorted_tanks.at(
-                                                                                                              i)->health /
+                                                                                                              i).health /
                                                                                                        (double) TANK_MAX_HEALTH))),
                         health_bar_end_x, health_bar_end_y, GREENMASK);
         }
